fcddb7c3d1aa3b76895a67b2c8d8acec4dc64841
rberr <berragrizlene1977@gmail.com>
2024-05-07 11:59:06 +0200

    modifier fonctionne à moitier

--- Controllers/MedicamentController.cs
+++ Controllers/MedicamentController.cs
@@ -1,27 +1,76 @@
 ﻿using Médicaments.Models.Dao;
 using Médicaments.Models.MesExceptions;
+using Médicaments.Models.Metier;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Médicaments.Controllers
 {
-    public class MedicamentController : Controller
-    {
-        public IActionResult Index()
-        {
-            System.Data.DataTable mesMedicaments = null;
+	public class MedicamentController : Controller
+	{
+		public IActionResult Index()
+		{
+			System.Data.DataTable mesMedicaments = null;
 
-            try
-            {
-                mesMedicaments = ServiceMedicament.GetTousLesMedicaments();
+			try
+			{
+				mesMedicaments = ServiceMedicament.GetTousLesMedicaments();
 
-            }
-            catch (MonException e)
-            {
-                ModelState.AddModelError("Erreur", "Erreur lors de la récupération des médicaments : " + e.Message);
-            }
+			}
+			catch (MonException e)
+			{
+				ModelState.AddModelError("Erreur", "Erreur lors de la récupération des médicaments : " + e.Message);
+			}
 
-            return View(mesMedicaments);
-        }
+			return View(mesMedicaments);
+		}
 
-    }
+		public IActionResult Modifier(string id)
+
+		{
+			Prescrire mesPrescriptions = null;
+
+			try
+			{
+
+				mesPrescriptions = ServiceMedicament.GetmesPrescriptions(id);
+
+				return View(mesPrescriptions);
+
+			}
+
+			catch (MonException e)
+			{
+
+				return NotFound();
+
+
+			}
+
+		}
+
+		[HttpPost]
+		public IActionResult Modifier(Prescrire unP)
+
+		{
+			try
+			{
+
+
+				ServiceMedicament.UpdatePrescription(unP);
+
+				return View();
+
+			}
+
+			catch (MonException e)
+			{
+
+				return NotFound();
+
+			}
+
+		}
+
+
+	}
 }
--- Models/Dao/ServiceMedicament.cs
+++ Models/Dao/ServiceMedicament.cs
@@ -1,54 +1,87 @@
 ﻿using System.Data;
 using Médicaments.Models.MesExceptions;
 using Médicaments.Models.Persistance;
+using Médicaments.Models.Metier;
+
 
 namespace Médicaments.Models.Dao
 {
-    public class ServiceMedicament
-    {
-        public static DataTable GetTousLesMedicaments()
-        {
-            DataTable mesMedicaments;
-            Serreurs er = new Serreurs("Erreur sur la lecture des médicaments.", "ServiceMedicament.GetTousLesMedicaments()");
-            try
-            {
-                String req = "SELECT medicament.id_medicament, medicament.id_famille, medicament.depot_legal, medicament.nom_commercial, medicament.effets, medicament.contre_indication, medicament.prix_echantillon, famille.lib_famille, dosage.qte_dosage, dosage.unite_dosage, prescrire.posologie, type_individu.lib_type_individu "
-                    + " FROM medicament " 
-                    + " JOIN famille ON medicament.id_famille = famille.id_famille "
-                    + " JOIN prescrire ON medicament.id_medicament = prescrire.id_medicament "
-                    + " JOIN dosage ON prescrire.id_dosage = dosage.id_dosage "
-                    + " JOIN type_individu ON prescrire.id_type_individu = type_individu.id_type_individu ";
+	public class ServiceMedicament
+	{
+		public static DataTable GetTousLesMedicaments()
+		{
+			DataTable mesMedicaments;
+			Serreurs er = new Serreurs("Erreur sur la lecture des médicaments.", "ServiceMedicament.GetTousLesMedicaments()");
+			try
+			{
+				String req = "SELECT medicament.id_medicament, medicament.id_famille, medicament.depot_legal, medicament.nom_commercial, medicament.effets, medicament.contre_indication, medicament.prix_echantillon, famille.lib_famille, dosage.qte_dosage, dosage.unite_dosage, prescrire.posologie, type_individu.lib_type_individu "
+					+ " FROM medicament "
+					+ " JOIN famille ON medicament.id_famille = famille.id_famille "
+					+ " JOIN prescrire ON medicament.id_medicament = prescrire.id_medicament "
+					+ " JOIN dosage ON prescrire.id_dosage = dosage.id_dosage "
+					+ " JOIN type_individu ON prescrire.id_type_individu = type_individu.id_type_individu ";
 
-                mesMedicaments = DBInterface.Lecture(req, er);
-            }
-            catch (MonException e)
-            {
-                throw new MonException(er.MessageUtilisateur(), er.MessageApplication(), e.Message);
-            }
-            return mesMedicaments;
-        }
+				mesMedicaments = DBInterface.Lecture(req, er);
+			}
+			catch (MonException e)
+			{
+				throw new MonException(er.MessageUtilisateur(), er.MessageApplication(), e.Message);
+			}
+			return mesMedicaments;
+		}
 
-        public static DataTable GetMedicamentById(int id)
-        {
-            DataTable mesMedicaments;
-            Serreurs er = new Serreurs("Erreur sur la lecture des médicaments.", "ServiceMedicament.GetMedicamentById()");
-            try
-            {
-                String req = "SELECT medicament.id_medicament, medicament.id_famille, medicament.depot_legal, " +
-                                    "medicament.nom_commercial, medicament.effets, medicament.contre_indication, " +
-                                    "medicament.prix_echantillon, famille.lib_famille " +
-                                    "FROM medicament " +
-                                    "JOIN famille ON medicament.id_famille = famille.id_famille " +
-                                    "WHERE medicament.id_medicament = " + id;
 
-                mesMedicaments = DBInterface.Lecture(req, er);
-                return mesMedicaments;
-            }
-            catch (MonException e)
-            {
-                throw new MonException(er.MessageUtilisateur(), er.MessageApplication(), e.Message);
-            }
-        }
-    }
+
+		public static Prescrire GetmesPrescriptions(String id)
+		{
+			DataTable dt;
+			Prescrire mesPrescriptions = null;
+			Serreurs er = new Serreurs("Erreur sur lecture des médicaments.", "Medicament");
+			try
+			{
+				String mysql = "SELECT * ";
+				mysql += "FROM prescrire ";
+				mysql += "WHERE id_medicament = " + id;
+				dt = DBInterface.Lecture(mysql, er);
+				if (dt.IsInitialized && dt.Rows.Count > 0)
+				{
+					mesPrescriptions = new Prescrire();
+					DataRow dataRow = dt.Rows[0];
+					mesPrescriptions.Id_medicament = int.Parse(dataRow[0].ToString());
+					mesPrescriptions.Id_dosage = int.Parse(dataRow[1].ToString());
+					mesPrescriptions.Id_type_individu = int.Parse(dataRow[2].ToString());
+					mesPrescriptions.Posologie = dataRow[3].ToString();
+
+					return mesPrescriptions;
+				}
+
+				else
+					return null;
+			}
+			catch (MonException e)
+			{
+				throw new MonException(er.MessageUtilisateur(), er.MessageApplication(), e.Message);
+			}
+		}
+
+		public static void UpdatePrescription(Prescrire unP)
+		{
+			Serreurs er = new Serreurs("Erreur sur la mise à jour du médicament.", "ServiceMedicament.UpdatePrescription()");
+			String req = "UPDATE Prescrire SET " +
+				"id_medicament = " + unP.Id_medicament +
+				", id_dosage = " + unP.Id_dosage +
+				", id_type_individu = " + unP.Id_type_individu +
+				", posologie = '" + unP.Posologie + "' " +
+				" WHERE id_medicament = " + unP.Id_medicament;
+			try
+			{
+				DBInterface.Execute_Transaction(req);
+			}
+			catch (MonException erreur)
+			{
+				throw erreur;
+			}
+		}
+	}
 }
 
